dc74aeecbbd94cd3c16b3af86503680b3b4871b1
spheroom <https://github.com/spheroom/Python_Basic.git>
2022-08-08 18:48:39 +0200

    First homework

---
+++ main.py
@@ -0,0 +1,20 @@
+# The Zen of Python, by Tim Peters
+import this
+
+# A webcomic of romance,sarcasm, math, and language.
+import antigravity
+
+
+# hello world feature that is not there anymore
+# import __hello__
+
+# easter egg in Python. It is a joke.
+# from __future__ import braces
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
